7f7560693fb9a98f09a62ad1a70510db2d72b246
Dsa.Dev_66 <122481070+ZeDogla66@users.noreply.github.com>
2023-10-14 10:00:36 -0300

    Melhoria aplicda em "insertion of data corrected"

--- insere.php
+++ insere.php
@@ -1,46 +1,9 @@
-<?php
-include "conexao.php";
-
-$matricula = $_POST['matricula'];
-$candidatoEscolhido = $_POST['candidato'];
-$sugestao = $_POST['sugestao'];
-
-// Calcula o hash SHA-3-256
-$hash = hash('sha3-256', $matricula);
-
-
-$sql = "SELECT * FROM resultado WHERE matricula = '$hash'";
-$result = $conn->query($sql);
-
-if ($result->num_rows == 0) {
-    
-    $sql = "INSERT INTO resultado (matricula, candidato, sugestao) VALUES (?, ?, ?)";
-    
-    $stmt = $conn->prepare($sql);
-    
-    if ($stmt !== false) {
-        $stmt->bind_param("sss", $hash, $candidatoEscolhido, $sugestao);
-        if ($stmt->execute()) {
-            $mensagem = "Dados inseridos com sucesso.";
-        } else {
-            $mensagem = "Erro ao inserir dados:<br> " . $stmt->error;
-        }
-    } else {
-        $mensagem = "Erro ao preparar a consulta.";
-    }
-} else {
-    $mensagem = "Esta matrícula já existe no banco de dados.";
-}
-
-$conn->close();
-?>
-
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resposta</title>
+    <title>Resultado da Inserção</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -51,19 +14,50 @@ $conn->close();
         <div class="barra-horizontal"></div>
     </header>
     <div class="content">
-        <h1>Resposta do Processamento</h1>
+    <h1>Resposta do Processamento</h1>
         <p>
-            <?php
-            if (isset($mensagem)) {
-                if (strpos($mensagem, "sucesso") !== false) {
-                    echo '<span style="color: green;">' . $mensagem . '</span>';
-                } else {
-                    echo '<span style="color: red;">' . $mensagem . '</span>';
-                }
+        <?php
+        include "conexao.php";
+
+        $matricula = $_POST['matricula'];
+        $candidatoEscolhido = $_POST['candidato'];
+        $sugestao = $_POST['sugestao'];
+        
+        // Calcula o hash SHA-1
+        $hash = hash('sha3-256', $matricula);
+        
+        // Verifica se a matrícula já existe no banco de dados
+        $sql = "SELECT * FROM resultado WHERE matricula = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $hash);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows == 0) {
+            // A matrícula não existe, então podemos inserir no banco de dados
+            $sql = "INSERT INTO resultado (matricula, candidato, sugestao) VALUES (?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sss", $hash, $candidatoEscolhido, $sugestao);
+            if ($stmt->execute()) {
+               
+            } else {
+                echo "Erro ao inserir dados:<br> " . $stmt->error;
             }
-            ?>
+        } else {
+            
+        }
+        
+
+        if ($result->num_rows == 0) {
+            echo "<p class='success-message'>Dados inseridos com sucesso.</p>";
+        } else {
+            echo "<p class='error-message'>Esta matrícula já existe no banco de dados.</p>";
+        }
+
+        $conn->close();
+        ?>
         </p>
         <center><a href="index.php" class="btn">Retornar à Página Inicial</a></center>
-    </div>
 </body>
 </html>
+
